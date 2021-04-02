3f22600172af971cdedeba1e2aea1acba2fc0ad6
shirin <yuda.pratama37@gmail.com>
2021-04-02 21:43:46 +0700

    finishing social link

--- js/theme.js
+++ js/theme.js
@@ -12,6 +12,7 @@ socialButton.onclick = () => {
     return s = "y";
 }
 
+/** theme switch handler */
 let themeCheckbox = document.querySelector('input[name=theme]');
 
 themeCheckbox.addEventListener('change', function() {
@@ -26,9 +27,37 @@ themeCheckbox.addEventListener('change', function() {
     }
 });
 
+/** adding transition to all element */
 let trans = () => {
     document.documentElement.classList.add('transition');
     setTimeout(() => {
         document.documentElement.classList.remove('transition');
     }, 1000);
 }
+
+/** copy to clipboard function */
+function copyStringToClipboard (str) {
+    // Create new element
+    var el = document.createElement('textarea');
+    // Set value (string to be copied)
+    el.value = str;
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand('copy');
+    // Remove temporary element
+    document.body.removeChild(el);
+ }
+
+ const discord = document.querySelector('#discord');
+ discord.addEventListener('click', function(){
+     
+    copyStringToClipboard("TypicalNob-#3733"); 
+    alert("Text copied to clipboard");
+ })
+   
+ 
